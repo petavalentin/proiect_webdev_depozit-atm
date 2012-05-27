cc298febb67e4694b6750f4d35cf59a49b0a8e98
Liviu HIRTAN <liviuhirtan@yahoo.com>
2012-05-27 17:47:17 +0200

    first project layout

---
+++ depozit.php
@@ -0,0 +1,36 @@
+<html>
+	<head>
+		<title>Gestiune ATM</title>
+		<link rel="stylesheet" type="text/css" href="my_style.css" />
+	</head>
+	
+	<body>
+		<div class="container">
+			<div class="header">
+				<h1 class="header">Depozit ATM</h1>
+			</div>
+			
+			<div class="left">
+				<ul>
+					<li><a href="depozit.php">Home</a></li>
+					<li><a href="#Stoc">Stoc</a></li>
+					<li><a href="#Info">Info produs</a></li>
+					<li><a href="#Adaugare">Adaugare</a></li>
+					<li><a href="#Predare">Predare</a></li>
+					<li><a href="#Contact">Contact</a></li>
+				</ul>
+			</div>
+			
+			<div class="content">
+				<h2>Bine ati venit!</h2>
+				<p>Acesta aplicatie web este utila pentru gestiunea produselor dintr-un depozit.</p>
+				<p>In continuare sunt prezentate sectiunile aplicatiei:</p>
+			</div>
+			
+			<div class="footer">
+				Copyright by ATM-Grenoble Team.
+			</div>
+		</div>
+	</body>
+</html>
+
